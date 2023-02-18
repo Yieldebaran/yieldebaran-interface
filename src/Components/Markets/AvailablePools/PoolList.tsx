00d9f6ca11b9a74e63154809ce3f6e10ce7a7d19
34x4p08 <34x4p08@gmail.com>
2023-02-18 17:33:44 -0300

    Update /public

--- src/Components/Markets/AvailablePools/PoolList.tsx
+++ src/Components/Markets/AvailablePools/PoolList.tsx
@@ -3,7 +3,6 @@ import '../style.css';
 import PoolRow from './PoolRow';
 import ReactTooltip from 'react-tooltip';
 import { useYieldebaranDataContext } from '../../../Types/appDataContext';
-import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 
 interface Props {
@@ -30,19 +29,6 @@ const PoolList: React.FC<Props> = (props: Props) => {
                     <th className='market-header-title'>Asset</th>
                     <th className='market-header-title'>
                         APY
-                        <Tippy content={
-                            <div style={{width: "12rem"}}>Learn about APR{' '}
-                                <a className="a" target="_blank" rel="noreferrer"
-                                   href="https://docs.hundred.finance/protocol-governance/hnd-staking-and-locking/boosting-apr-with-vehnd">here</a>
-                            </div>} interactive={true}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#015CB1"
-                                 className="info-circle" viewBox="0 0 16 16"
-                                 style={{userSelect: "none", outline: "none", cursor: "pointer"}}>
-                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                <path
-                                    d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-                            </svg>
-                        </Tippy>
                     </th>
 
                     <th className='market-header-title'>Total assets</th>
