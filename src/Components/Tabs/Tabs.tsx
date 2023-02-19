4373e3ea67ac01e0bab2f4f8e485172c760eed27
Spiridon Robakidze <spiridonelectro@gmail.com>
2023-02-20 00:17:52 +0100

    fix packages

--- src/Components/Tabs/Tabs.tsx
+++ src/Components/Tabs/Tabs.tsx
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export const Tabs: React.FC = () => {};
+export const Tabs: React.FC = () => null;
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Web3Provider } from '@ethersproject/providers';
 import { Web3ReactProvider } from '@web3-react/core';
-
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
 
 function getLibrary(provider: any): Web3Provider {
-  const library = new Web3Provider(provider)
-  library.pollingInterval = 12000
-  return library
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 12000;
+  return library;
 }
 
-ReactDOM.render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
   <React.StrictMode>
     <Web3ReactProvider getLibrary={getLibrary}>
       <App />
     </Web3ReactProvider>
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
